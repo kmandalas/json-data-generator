7adce6e22289ebecb3a523e28f776310d1d4a10d
Kyriakos Mandalas <Kyriakos.Mandalas@IRIworldwide.com>
2017-12-18 18:28:52 +0200

    added the ability to send message with key to kafka

--- src/main/java/net/acesinc/data/json/generator/log/KafkaLogger.java
+++ src/main/java/net/acesinc/data/json/generator/log/KafkaLogger.java
@@ -9,13 +9,18 @@ import java.io.IOException;
 import java.util.Map;
 import java.util.Properties;
 import java.util.concurrent.ExecutionException;
-import net.acesinc.data.json.util.JsonUtils;
+
+import org.apache.commons.lang.StringUtils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import net.acesinc.data.json.util.JsonUtils;
 
 /**
  *
@@ -33,14 +38,15 @@ public class KafkaLogger implements EventLogger {
     private final boolean flatten;
     private final Properties props = new Properties();
     private JsonUtils jsonUtils;
+    private String keyAttribute;
     
     public KafkaLogger(Map<String, Object> props) {
         String brokerHost = (String) props.get(BROKER_SERVER_PROP_NAME);
         Integer brokerPort = (Integer) props.get(BROKER_PORT_PROP_NAME);
-        
-        this.props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,brokerHost + ":" + brokerPort.toString());
-        this.props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
-        this.props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+
+        this.props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, brokerHost + ":" + brokerPort.toString());
+        this.props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        this.props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         
         producer = new KafkaProducer<>(this.props);
         
@@ -56,7 +62,13 @@ public class KafkaLogger implements EventLogger {
         } else {
             this.flatten = false;
         }
-        
+
+        if (props.get("keyAttribute") != null) {
+            this.keyAttribute = (String) props.get("keyAttribute");
+        } else {
+            this.keyAttribute = null;
+        }
+
         this.jsonUtils = new JsonUtils();
     }
 
@@ -77,8 +89,21 @@ public class KafkaLogger implements EventLogger {
                 return;
             }
         }
-        
-        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topic, output);
+
+        ProducerRecord<String, String> producerRecord = null;
+        if (StringUtils.isNotBlank(keyAttribute)) {
+            try {
+                ObjectMapper mapper = new ObjectMapper();
+                Map<String, Object> stringObjectMap = mapper.readValue(event, Map.class);
+                String key = (String) stringObjectMap.get(keyAttribute);
+                producerRecord = new ProducerRecord<>(topic, key, output);
+            } catch (IOException ex) {
+                log.error("Error while extracting kafka key vale", ex);
+            }
+        } else {
+            producerRecord = new ProducerRecord<>(topic, output);
+        }
+
         if (sync) {
             try {
                 producer.send(producerRecord).get();
